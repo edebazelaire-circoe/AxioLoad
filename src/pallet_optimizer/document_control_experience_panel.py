95a4473c19274d0514d0c8edf7ce0bc3be23f148
BaptisteOlv <b.olivier@circoe.com>
2026-08-01 00:19:57 +0200

    Charge la navigation documentaire optimisée

--- src/pallet_optimizer/document_control_experience_panel.py
+++ src/pallet_optimizer/document_control_experience_panel.py
@@ -5,9 +5,12 @@ from typing import Any
 
 from fastapi.templating import Jinja2Templates
 
-_STYLE = b'<link rel="stylesheet" href="/static/document_control_experience.css?v=0.18.0">'
-_SCRIPT = b'<script src="/static/document_control_experience.js?v=0.18.0"></script>'
-_PERMISSION_SCRIPT = b'<script src="/static/document_control_permission_ui.js?v=0.18.0"></script>'
+_STYLE = b'<link rel="stylesheet" href="/static/document_control_experience.css?v=0.19.1">'
+_SCRIPT = b'<script src="/static/document_control_experience_v2.js?v=0.19.1"></script>'
+_PERMISSION_SCRIPT = b'<script src="/static/document_control_permission_ui.js?v=0.19.1"></script>'
+_OLD_STYLE = b'<link rel="stylesheet" href="/static/document_control_experience.css?v=0.18.0">'
+_OLD_SCRIPT = b'<script src="/static/document_control_experience.js?v=0.18.0"></script>'
+_OLD_PERMISSION_SCRIPT = b'<script src="/static/document_control_permission_ui.js?v=0.18.0"></script>'
 _original_template_response: Callable[..., Any] | None = None
 
 
@@ -22,10 +25,16 @@ def install_document_control_experience_injection() -> None:
         response = _original_template_response(self, *args, **kwargs)
         body = getattr(response, "body", b"")
         if b'id="open-settings"' in body:
-            if _STYLE not in body:
-                body = body.replace(b"</head>", _STYLE + b"</head>")
-            for script in (_SCRIPT, _PERMISSION_SCRIPT):
-                body = body.replace(script, b"")
+            for asset in (
+                _OLD_STYLE,
+                _OLD_SCRIPT,
+                _OLD_PERMISSION_SCRIPT,
+                _STYLE,
+                _SCRIPT,
+                _PERMISSION_SCRIPT,
+            ):
+                body = body.replace(asset, b"")
+            body = body.replace(b"</head>", _STYLE + b"</head>")
             body = body.replace(b"</body>", _SCRIPT + _PERMISSION_SCRIPT + b"</body>")
             response.body = body
             response.headers["content-length"] = str(len(body))

3e5472619dec9f5875b7295e9ed53437f36add1b
BaptisteOlv <b.olivier@circoe.com>
2026-08-03 13:37:48 +0200

    Aligne la version du correctif

--- src/pallet_optimizer/__init__.py
+++ src/pallet_optimizer/__init__.py
@@ -31,7 +31,7 @@ from .prompt_center_system import install_prompt_center_system
 from .scaling import install_unlimited_item_count
 from .super_admin_routes import install_super_admin_routes
 
-__version__ = "0.19.2"
+__version__ = "0.19.3"
 
 install_super_admin_routes()
 install_password_reset_system()

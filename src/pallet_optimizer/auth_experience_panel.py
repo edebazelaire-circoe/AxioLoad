652dc7149281539acb26d868804a5ec506906be0
BaptisteOlv <b.olivier@circoe.com>
2026-08-03 12:40:53 +0200

    Restaure l’injection d’authentification de la PR 23

--- src/pallet_optimizer/auth_experience_panel.py
+++ src/pallet_optimizer/auth_experience_panel.py
@@ -5,18 +5,10 @@ from typing import Any
 
 from fastapi.templating import Jinja2Templates
 
-from .version import APP_VERSION
-
-_STYLE = f'<link rel="stylesheet" href="/static/auth_experience.css?v={APP_VERSION}">'.encode()
-_SCRIPT = f'<script src="/static/auth_experience.js?v={APP_VERSION}"></script>'.encode()
-_STABILITY = f'<script src="/static/stability_controls.js?v={APP_VERSION}"></script>'.encode()
-_LEGACY_ASSETS = (
-    b'<link rel="stylesheet" href="/static/auth_experience.css?v=0.18.0">',
-    b'<script src="/static/auth_experience.js?v=0.18.0"></script>',
-    b'<link rel="stylesheet" href="/static/auth_experience.css?v=0.19.1">',
-    b'<script src="/static/auth_experience.js?v=0.19.1"></script>',
-    b'<script src="/static/session_controls_guard.js?v=0.22.1"></script>',
-)
+_STYLE = b'<link rel="stylesheet" href="/static/auth_experience.css?v=0.19.1">'
+_SCRIPT = b'<script src="/static/auth_experience.js?v=0.19.1"></script>'
+_OLD_STYLE = b'<link rel="stylesheet" href="/static/auth_experience.css?v=0.18.0">'
+_OLD_SCRIPT = b'<script src="/static/auth_experience.js?v=0.18.0"></script>'
 _original_template_response: Callable[..., Any] | None = None
 
 
@@ -31,10 +23,10 @@ def install_auth_experience_injection() -> None:
         response = _original_template_response(self, *args, **kwargs)
         body = getattr(response, "body", b"")
         if b'id="open-settings"' in body or b'id="login-form"' in body:
-            for asset in (*_LEGACY_ASSETS, _STYLE, _SCRIPT, _STABILITY):
+            for asset in (_OLD_STYLE, _OLD_SCRIPT, _STYLE, _SCRIPT):
                 body = body.replace(asset, b"")
             body = body.replace(b"</head>", _STYLE + b"</head>")
-            body = body.replace(b"</body>", _SCRIPT + _STABILITY + b"</body>")
+            body = body.replace(b"</body>", _SCRIPT + b"</body>")
             response.body = body
             response.headers["content-length"] = str(len(body))
         return response
